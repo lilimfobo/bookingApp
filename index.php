7bd52ccfd4a875c1a5e32b5bc089ba0977528d40
Lili <102562771+lilimfobo@users.noreply.github.com>
2022-11-24 23:42:21 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hotel Bookings</title>
+    <title>Booking App</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
-    <h1 class="text-center my-4 py-4">Book It!</h1>
+    <h1 class="text-center my-4 py-4">Quick Bookings</h1>
 
-    <h4 class="text-center">Compare trip prices fast.</h4>
+    <h4 class="text-center">Compare trip prices easier!</h4>
     <div class="w-50 m-auto">
 
 
