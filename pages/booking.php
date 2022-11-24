95438b03ce511f214d5d23ccba0008accaf4affc
Lili <102562771+lilimfobo@users.noreply.github.com>
2022-11-24 23:08:40 +0200

    Create booking.php

---
+++ pages/booking.php
@@ -0,0 +1,44 @@
+<?php
+
+require ".../classes/class.php";
+
+session_start();
+
+if (isset($_POST['confirm-booking'])) {
+
+    $name = $_POST['name'];
+    $surname = $_POST['lastName'];
+    $email  = $_POST['email'];
+
+  }
+
+   // --- Creating CSV file ---//
+
+   $headers = ["Name", "Surname", "Email", "hotelID", "HotelName", "inDate", "outDate", "Costpernight"];
+
+   $data = [$name, $surname, $email , $selectedHotel->getId() ,$selectedHotel->getName() ,$inDate, $outDate, $selectedHotel->getCostPerNight()];
+ 
+ 
+   //---- Open CSV file ---//
+ 
+   $fh = fopen("file.csv", "w");
+ 
+ 
+   //---- Create headers ---//
+ 
+   fputcsv($fh, $headers);
+ 
+ 
+   //--- populate the data ---//
+ 
+   foreach ($data as $fields) {
+     fputcsv($fh,$fields);
+   }
+ 
+ 
+   //---- Close CSV file ---//
+   fclose($fh);
+
+
+
+?>
